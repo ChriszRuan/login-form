2dc12fe89f8c0b2a93dc58c35c8bd90d45b59a37
ChriszRuan <masteruan.moreira@gmail.com>
2026-09-21 15:21:11 -0300

    Validação formulário JS

---
+++ script.js
@@ -0,0 +1,21 @@
+let inputUsername = document.getElementById("username");
+let inputPassword = document.getElementById("password");
+let loginButton = document.getElementById("login-button");
+let forgotPasswordButton = document.getElementById("redefinir-senha");
+
+loginButton.addEventListener("click", function() {
+    let username = inputUsername.value;
+    let password = inputPassword.value;
+
+    if (inputUsername.value === "" || inputPassword.value === "") {
+        alert("Por favor, preencha todos os campos.");
+    } else if (inputUsername.value.length < 3) {
+        alert("O nome do usuário deve conter no mínimo 3 caracteres.");
+    } else if (inputPassword.value.length < 6) {
+        alert("A senha deve conter no mínimo 6 caracteres.");
+    } else {
+        alert("Login bem-sucedido!");
+    }
+
+
+});
